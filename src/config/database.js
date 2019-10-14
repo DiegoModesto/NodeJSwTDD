7e072ffe7f2bfd0ab227e9404e4a4cf33265104c
Diego Modesto <diegosanches89@gmail.com>
2019-10-14 09:32:41 -0300

    Added SSL to connect at database

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,10 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  },
   storage: './__tests__/database.sqlite',
   logging: false,
   define: {
